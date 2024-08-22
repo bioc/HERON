90fa1a285469b465ee632e170c6eac48fc196cc2
sjmcilwain <sjmcilwain@wisc.edu>
2024-08-22 14:43:52 -0500

    Updated to make compliant with CRAN and Bioconductor checks

--- R/calcProbePvaluesW.R
+++ R/calcProbePvaluesW.R
@@ -11,25 +11,30 @@ calcProbePValuesW<-function(c_mat, colData_in, sd_shift,
     }
 }
 
-#' Title
+#' Calculate Probe p-values using a two-sample wilcoxon test
 #'
-#' @param probe_mat
-#' @param colData_in
-#' @param exact
-#' @param abs_shift
+#' @param probe_mat numeric matrix or data.frame of values
+#' @param colData_in design data.frame
+#' @param exact a logical indicating whether an exact p-value should be
+#' computed (see wilcox.test for details)
+#' @param abs_shift absolute shift to use when calculating p-values
 #'
-#' @return
+#' @return matrix of p-values on the post columns defined in the colData matrix
 #' @export
 #'
 #' @importFrom stats wilcox.test
 #'
-#' @noRd
+#' @examples
+#' data(heffron2021_wuhan)
+#' colData_wu <- colData(heffron2021_wuhan)
+#' pval_res <- calcProbePValuesWUnpaired(assay(heffron2021_wuhan), colData_wu)
 calcProbePValuesWUnpaired<-function(
     probe_mat,
     colData_in,
     exact = NULL,
     abs_shift = 0
 ) {
+    message("exact:", exact)
     if (is.na(abs_shift)) {abs_shift <- 0}
     pre_cols <- rownames(colData_in)[tolower(colData_in$visit) == "pre"]
     post_cols <- rownames(colData_in)[tolower(colData_in$visit) == "post"]
@@ -38,13 +43,18 @@ calcProbePValuesWUnpaired<-function(
     rownames(ans) <- rownames(probe_mat)
     for (probe in rownames(ans)) {
         x_val <- as.numeric(probe_mat[probe,pre_cols])
+        cexact <- exact # Prevent a warning if there are ties
+        if (is.null(cexact) && any(duplicated(x_val))) {
+            cexact <- FALSE
+        }
         for (post_col in post_cols) {
             y_val <- as.numeric(probe_mat[probe, post_col])
+
             test_res <- wilcox.test(
                 x = x_val,
                 mu = y_val - abs_shift,
                 paired = FALSE,
-                exact = exact,
+                exact = cexact,
                 alternative = "less"
             )
             ans[probe, post_col] <- test_res$p.value
